2d23f5f1a9580a1257543953a55b5b4d7915666a
leeann22 <126453766+leeann22@users.noreply.github.com>
2025-10-29 13:44:28 +0800

    Added testing file with basic utility functions.

---
+++ testing.js
@@ -0,0 +1,18 @@
+/**
+ * Dummy utilities.
+ *
+ * Exports:
+ * - add(a, b): number
+ * - multiply(a, b): number
+ */
+
+// Simple synchronous utilities
+function add(a, b) {
+	return a + b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+
